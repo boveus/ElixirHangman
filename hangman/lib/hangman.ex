b460389a73aac7bb4f5e37fdc7fff31b8d108863
Brandon Stewart <brandon.stewart88@gmail.com>
2019-03-19 17:09:35 -0400

    remove inflex dependency

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -3,13 +3,7 @@ defmodule Hangman do
     IO.puts Dictionary.random_word()
   end
 
-
   def word do
     Dictionary.random_word()
-  end
-
-  def plural_word do
-    word()
-    |> Inflex.pluralize()
   end
 end

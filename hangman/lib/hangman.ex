a328f505c6ab3e1ffc82754dbff0ca3f2e40a1e3
Brandon Stewart <brandon.stewart88@gmail.com>
2019-03-19 17:08:53 -0400

    add inflex dependency

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -2,4 +2,14 @@ defmodule Hangman do
   def hello do
     IO.puts Dictionary.random_word()
   end
+
+
+  def word do
+    Dictionary.random_word()
+  end
+
+  def plural_word do
+    word()
+    |> Inflex.pluralize()
+  end
 end
